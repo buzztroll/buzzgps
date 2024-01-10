d67a6f4d8743dc1c788f73bfd0dd5769fc640c5a
John Bresnahan <buzztroll@gmail.com>
2024-01-10 08:39:01 -1000

    Added test framework

--- tests/test_blocking.c
+++ tests/test_blocking.c
@@ -2,18 +2,28 @@
 #include <string.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
 #include <cmocka.h>
 
+#include <buzz_gps.h>
 
-static void null_test_success(void **state)
+
+
+static void test_bad_path(void **state)
 {
-    (void) state;
+   int rc;
+   buzz_gps_handle_t gps_h;
+
+   rc = buzz_gps_init(&gps_h, "/X/XXX/bad", 0, 0);
+   assert_int_not_equal(BUZZ_GPS_SUCCESS, rc);
 }
  
 int main(int argc, char ** argv)
 {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(null_test_success),
+        cmocka_unit_test(test_bad_path),
     };
  
     return cmocka_run_group_tests(tests, NULL, NULL);
